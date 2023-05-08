066e409bf32b7c451c311a335a826f95c16fe951
Maikel Bos <maikel.bos0@gmail.com>
2023-05-08 10:47:40 +0200

    Add empty examples project

---
+++ src/VDT.Core.XmlConverter.Examples/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace VDT.Core.XmlConverter.Examples {
+    public class Program {
+        public static async Task Main(string[] args) {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
