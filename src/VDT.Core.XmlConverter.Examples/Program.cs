6d72e21871d3d787e1763b1fffbd93559e812d65
Maikel Bos <maikel.bos0@gmail.com>
2026-02-06 13:40:00 +0100

    Switch to top-level main

--- src/VDT.Core.XmlConverter.Examples/Program.cs
+++ src/VDT.Core.XmlConverter.Examples/Program.cs
@@ -1,13 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Threading.Tasks;
+using VDT.Core.XmlConverter.Examples;
 
-namespace VDT.Core.XmlConverter.Examples {
-    public class Program {
-        public static async Task Main(string[] args) {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
